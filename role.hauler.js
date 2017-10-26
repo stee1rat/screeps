7fa5a821d405b3dbb7d9e7522ac4eb055be1e968
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-26 21:36:38 +0300

    update

--- role.hauler.js
+++ role.hauler.js
@@ -3,20 +3,32 @@ var roleHauler = {
     run: function(creep) {
 
       if (creep.carry.energy == 0) {
-        var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        let source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: structure =>
                     structure.structureType == STRUCTURE_CONTAINER &&
                     structure.store[RESOURCE_ENERGY] > 0
         })
 
-        var withdraw = creep.withdraw(source, RESOURCE_ENERGY)
+        if (source == null) {
+          let x1 = Memory.parkingArea[0][0]
+          let x2 = Memory.parkingArea[0][1]
+          let y1 = Memory.parkingArea[1][0]
+          let y2 = Memory.parkingArea[1][1]
+
+          for (i = x1; i <= x2; i++) {
+            for (j = y1; j <= y2; j++) {
+              console.log(Game.map.getTerrainAt(i, j, creep.room.name))
+          }
+
+        }
+
+        let withdraw = creep.withdraw(source, RESOURCE_ENERGY)
 
         if (withdraw == ERR_NOT_IN_RANGE) {
-          let result = creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}})
-          console.log(result)
+          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}})
         }
       } else {
-        var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_SPAWN ||
                             structure.structureType == STRUCTURE_EXTENSION) &&
