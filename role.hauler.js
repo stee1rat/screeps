e4f04a2e9196a6123cc2b9c259fea3a37615fbc2
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-26 21:48:55 +0300

    parking area for haulers

--- role.hauler.js
+++ role.hauler.js
@@ -15,18 +15,19 @@ var roleHauler = {
           let y1 = Memory.parkingArea[1][0];
           let y2 = Memory.parkingArea[1][1];
 
-          for (let x = x1; x <= x2; x++) {
-            for (let y = y1; y <= y2; y++) {
-              let terrainType = Game.map.getTerrainAt(x, y, creep.room.name);
-              if (terrainType != 'wall') {
-                if (creep.room.lookForAt(LOOK_CREEPS, x, y).length == 0) {
-                  creep.moveTo(x, y, {visualizePathStyle: {stroke: '#ffaa00'}});
-                  break;
+          if (creep.pos.x < x1 or creep.pos.x > x2 or creep.pos.y < y1 or creep.pos.y > y2) {
+            for (let x = x1; x <= x2; x++) {
+              for (let y = y1; y <= y2; y++) {
+                let terrainType = Game.map.getTerrainAt(x, y, creep.room.name);
+                if (terrainType != 'wall') {
+                  if (creep.room.lookForAt(LOOK_CREEPS, x, y).length == 0) {
+                    creep.moveTo(x, y, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    break;
+                  }
                 }
               }
             }
           }
-
         }
 
         let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
