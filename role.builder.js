73fe8ca9f65a2533857f2b18c59d7f6bde9a22fd
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-26 22:07:44 +0300

    move parking function into its own module

--- role.builder.js
+++ role.builder.js
@@ -1,50 +1,33 @@
 var roleBuilder = {
 
-    run: function(creep) {
-	    //if (creep.carry.energy < creep.carryCapacity) {
-      if (creep.carry.energy == 0 || creep.memory.harvesting) {
-        //var sources = creep.room.find(FIND_SOURCES);
-        var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        var harvest = creep.harvest(source)
+  customFunctions = require('custom.functions');
 
-        if (harvest == ERR_NOT_IN_RANGE)
-          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+  run: function(creep) {
+    if (creep.carry.energy == 0 || creep.memory.harvesting) {
+      let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      let harvest = creep.harvest(source)
 
-        if (harvest == OK)
-          creep.memory.harvesting = true
+      if (harvest == ERR_NOT_IN_RANGE)
+        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
 
-        if (creep.carry.energy == creep.carryCapacity)
-          creep.memory.harvesting = false
+      if (harvest == OK)
+        creep.memory.harvesting = true
+
+      if (creep.carry.energy == creep.carryCapacity)
+        creep.memory.harvesting = false
+    }
+    else {
+      let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+      if (target != null) {
+        if (creep.build(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+      } else {
+        customFunctions.park(creep);
       }
-      else {
-          var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-          if (target != null) {
-              if (creep.build(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-              }
-          } else {
-            let x1 = Memory.parkingArea[0][0];
-            let x2 = Memory.parkingArea[0][1];
-            let y1 = Memory.parkingArea[1][0];
-            let y2 = Memory.parkingArea[1][1];
-
-            if (creep.pos.x < x1 || creep.pos.x > x2 ||
-                creep.pos.y < y1 || creep.pos.y > y2) {
-              for (let x = x1; x <= x2; x++) {
-                for (let y = y1; y <= y2; y++) {
-                  let terrainType = Game.map.getTerrainAt(x, y, creep.room.name);
-                  if (terrainType != 'wall') {
-                    if (creep.room.lookForAt(LOOK_CREEPS, x, y).length == 0) {
-                      creep.moveTo(x, y, {visualizePathStyle: {stroke: '#ffaa00'}});
-                      break;
-                    }
-                  }
-                }
-              }
-            }
-          }
-      }
+    }
 	}
+
 };
 
 module.exports = roleBuilder;
--- role.hauler.js
+++ role.hauler.js
@@ -1,4 +1,6 @@
 var roleHauler = {
+  
+    customFunctions = require('custom.functions');
 
     run: function(creep) {
 
@@ -10,25 +12,7 @@ var roleHauler = {
         })
 
         if (source == null) {
-          let x1 = Memory.parkingArea[0][0];
-          let x2 = Memory.parkingArea[0][1];
-          let y1 = Memory.parkingArea[1][0];
-          let y2 = Memory.parkingArea[1][1];
-
-          if (creep.pos.x < x1 || creep.pos.x > x2 ||
-              creep.pos.y < y1 || creep.pos.y > y2) {
-            for (let x = x1; x <= x2; x++) {
-              for (let y = y1; y <= y2; y++) {
-                let terrainType = Game.map.getTerrainAt(x, y, creep.room.name);
-                if (terrainType != 'wall') {
-                  if (creep.room.lookForAt(LOOK_CREEPS, x, y).length == 0) {
-                    creep.moveTo(x, y, {visualizePathStyle: {stroke: '#ffaa00'}});
-                    break;
-                  }
-                }
-              }
-            }
-          }
+          customFunctions.park(creep);
         }
 
         let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
