99297d9ec4713051832254ff3cc7686ccf591b17
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-27 11:09:23 +0300

    resolving errors

--- role.harvester.js
+++ role.harvester.js
@@ -1,7 +1,7 @@
 var roleHarvester = {
 
   run: function(creep) {
-    if (creep.carry.energy == 0 || creep.memory.harvesting) {
+    if (creep.carry.energy === 0 || creep.memory.harvesting) {
       var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
       var harvest = creep.harvest(source);
 
@@ -22,9 +22,9 @@ var roleHarvester = {
               (structure.structureType == STRUCTURE_SPAWN ||
                structure.structureType == STRUCTURE_EXTENSION) &&
                structure.energy < structure.energyCapacity
-      })
+      });
 
-      if (target != null) {
+      if (target !== null) {
         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(target, {
               visualizePathStyle: {stroke: '#ffffff'},
