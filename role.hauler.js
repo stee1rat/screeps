c0020a70d992df5ea6d7d2aa456acfae50b3a462
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-27 21:33:20 +0300

    updated hauler to find towers and pickup dropped energy

--- role.hauler.js
+++ role.hauler.js
@@ -1,32 +1,48 @@
 var roleHauler = {
+
   run: function(creep, customFunctions) {
-      if (creep.carry.energy == 0) {
-        let source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: structure =>
-                    structure.structureType == STRUCTURE_CONTAINER &&
-                    structure.store[RESOURCE_ENERGY] > 0
-        })
-        if (source == null) {
-          customFunctions.park(creep);
-        }
-        let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
+    if (creep.carry.energy === 0) {
+      // First the haulers look for dropped energy
+      let source = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY);
+      if (source !== null) {
+        let withdraw = creep.pickup(source);
         if (withdraw == ERR_NOT_IN_RANGE) {
           creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
       } else {
-        let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_EXTENSION) &&
-                        structure.energy < structure.energyCapacity;
-                }
-        })
-        if (target != null) {
-          if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-          }
+        // If no dropped energy lets widthdraw from container
+        let source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: structure =>
+                structure.structureType == STRUCTURE_CONTAINER &&
+                structure.store[RESOURCE_ENERGY] > 0
+        });
+
+        // If nothing found at all hauler parks
+        if (source === null) {
+          customFunctions.park(creep);
+        }
+
+        let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
+        if (withdraw == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
       }
+    } else {
+      let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          filter: (structure) => {
+              return (structure.structureType == STRUCTURE_SPAWN ||
+                      structure.structureType == STRUCTURE_EXTENSION ||
+                      structure.structureType == STRUCTURE_TOWER) &&
+                  structure.energy < structure.energyCapacity;
+          }
+      });
+
+      if (target !== null) {
+        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+      }
+    }
 	}
 };
 
