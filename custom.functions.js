5620be5a452cc4013159f6b95b64e4536ed942ae
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-29 18:02:10 +0300

    added customFunctions.getEnergy

--- custom.functions.js
+++ custom.functions.js
@@ -1,16 +1,52 @@
 module.exports = {
+
+  getEnergy: function(creep) {
+    // Dropped energy
+    let source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES,
+      { filter: resource => resource.resourceType == RESOURCE_ENERGY }
+    );
+
+    if (source !== null) {
+      let withdraw = creep.pickup(source);
+      if (withdraw == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, {
+          visualizePathStyle: {stroke: '#ffaa00'}, reusePath: 5
+        });
+      }
+    } else {
+      // Container energy
+      let source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          filter: structure =>
+              structure.structureType == STRUCTURE_CONTAINER &&
+              structure.store[RESOURCE_ENERGY] > 0
+      });
+
+      // If nothing found return null
+      if (source === null) {
+        return null;
+      }
+
+      let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
+      if (withdraw == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+    }
+    return OK;
+  },
+
   park: function(creep) {
     let x1 = Memory.parkingArea[0][0];
     let x2 = Memory.parkingArea[0][1];
     let y1 = Memory.parkingArea[1][0];
     let y2 = Memory.parkingArea[1][1];
+
     if (creep.pos.x < x1 || creep.pos.x > x2 ||
         creep.pos.y < y1 || creep.pos.y > y2) {
       for (let x = x1; x <= x2; x++) {
         for (let y = y1; y <= y2; y++) {
           let terrainType = Game.map.getTerrainAt(x, y, creep.room.name);
           if (terrainType != 'wall') {
-            if (creep.room.lookForAt(LOOK_CREEPS, x, y).length == 0) {
+            if (creep.room.lookForAt(LOOK_CREEPS, x, y).length === 0) {
               creep.moveTo(x, y, {visualizePathStyle: {stroke: '#ffaa00'}});
               break;
             }
--- role.hauler.js
+++ role.hauler.js
@@ -4,33 +4,8 @@ let roleHauler = {
 
   run: function(creep) {
     if (creep.carry.energy === 0) {
-      // First the haulers look for dropped energy
-      let source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES,
-          { filter: resource => resource.resourceType == RESOURCE_ENERGY }
-      );
-
-      if (source !== null) {
-        let withdraw = creep.pickup(source);
-        if (withdraw == ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
-      } else {
-        // If no dropped energy lets widthdraw from container
-        let source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: structure =>
-                structure.structureType == STRUCTURE_CONTAINER &&
-                structure.store[RESOURCE_ENERGY] > 0
-        });
-
-        // If nothing found at all hauler parks
-        if (source === null) {
-          customFunctions.park(creep);
-        }
-
-        let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
-        if (withdraw == ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
+      if (customFunctions.getEnergy(creep) == null) {
+        customFunctions.park(creep);
       }
     } else {
       let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
