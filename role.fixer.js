66b4930ba722afc22fb491631a02fd5c82325f3b
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-29 17:38:41 +0300

    fixer

--- role.fixer.js
+++ role.fixer.js
@@ -1,44 +1,48 @@
 var roleFixer = {
-    run: function(creep) {
-      if (creep.carry.energy == 0 || creep.memory.harvesting) {
-        var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
-        var harvest = creep.harvest(source)
 
-        if (harvest == ERR_NOT_IN_RANGE)
-          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}})
+  run: function(creep) {
+    if (creep.carry.energy === 0 || creep.memory.harvesting) {
+      let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      let harvest = creep.harvest(source);
 
-        if (harvest == OK)
-          creep.memory.harvesting = true
+      if (harvest == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
 
-        if (creep.carry.energy == creep.carryCapacity)
-          creep.memory.harvesting = false
+      if (harvest == OK) {
+        creep.memory.harvesting = true;
+      }
+
+      if (creep.carry.energy == creep.carryCapacity) {
+        creep.memory.harvesting = false;
+      }
+    } else {
+      if (!creep.memory.repairTaget) {
+        let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: structure => structure.hits < structure.hitsMax * 0.7 &&
+              structure.hits < 50000
+        });
+
+        if (target) {
+          creep.memory.repairTaget = target.id;
         }
-        else {
-          if (!creep.memory.repairTaget) {
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: structure => structure.hits < structure.hitsMax * 0.7
-            })
+      } else {
+        let target = Game.getObjectById(creep.memory.repairTaget);
 
-            if (target) {
-              creep.memory.repairTaget = target.id
-            }
+        if (target == []._) {
+          creep.memory.repairTaget = 0;
+        } else {
+          if (target.hits == target.hitsMax || target.hits > 50000) {
+            creep.memory.repairTaget = null;
           } else {
-            var target = Game.getObjectById(creep.memory.repairTaget)
-
-            if (target == []._) {
-              creep.memory.repairTaget = 0;
-            } else {
-              if (target.hits == target.hitsMax && target.hits < 50000) {
-                creep.memory.repairTaget = null
-              } else {
-                if (creep.repair(target) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-              }
+            if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
             }
           }
         }
       }
+    }
+  }
 };
 
 module.exports = roleFixer;
