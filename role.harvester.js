b1e131b74968ff952594a5b294c203853bebd007
Vasiliy Aboymov <aboymov@gmail.com>
2017-10-30 07:54:13 +0300

    fixed storage usage

--- role.harvester.js
+++ role.harvester.js
@@ -27,7 +27,7 @@ var roleHarvester = {
         target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: structure =>
                 structure.structureType == STRUCTURE_STORAGE &&
-                structure.store < structure.storeCapacity;
+                structure.store < structure.storeCapacity
         });
       }
       if (target !== null) {
