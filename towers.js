10f3be34ec7fa36d85e304909cca6460f3bdce9f
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-04 00:39:21 +0300

    towers

--- towers.js
+++ towers.js
@@ -32,8 +32,6 @@ var towers = {
         if(damagedStructure.length) {
           tower.repair(damagedStructure[0]);
           continue;
-        } else {
-          break;
         }
       }
     }
