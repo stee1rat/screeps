f1ae014ba8ebd2a99f5d32c3b17983bfb4e29fb4
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-04 00:01:40 +0300

    new hauler code

--- role.hauler.js
+++ role.hauler.js
@@ -1,23 +1,77 @@
 let roleHauler = {
 
   run: function(creep) {
+    /*let roomCreeps = creep.room.find(FIND_MY_CREEPS, {filter: c =>
+       c.memory.source && c.memory.role == 'hauler'
+    });
+    console.log(roomCreeps.length);*/
     if (creep.carry.energy === 0) {
-      creep.getEnergy();
-    } else {
-      let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-          filter: (structure) => {
-              return (structure.structureType == STRUCTURE_SPAWN ||
-                      structure.structureType == STRUCTURE_EXTENSION ||
-                      structure.structureType == STRUCTURE_TOWER) &&
-                  structure.energy < structure.energyCapacity;
+      if (!creep.memory.source) {
+        let source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+          filter: resource => resource.resourceType == RESOURCE_ENERGY
+        });
+        if (source === null) {
+          source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: structure =>
+              (structure.structureType == STRUCTURE_CONTAINER ||
+               structure.structureType == STRUCTURE_STORAGE) &&
+               structure.store[RESOURCE_ENERGY] > 0
+          });
+        }
+        if (source) {
+          creep.memory.source = source.id;
+        }
+      }
+      if (creep.memory.source) {
+        let source = Game.getObjectById(creep.memory.source);
+        // if dropped energy has been pickup up or decayed
+        // find another source
+        if (!source) {
+          creep.memory.source = null;
+          return;
+        }
+        // if the source is a dropped energy pick it up
+        if (source.resourceType) {
+          let pickup = creep.pickup(source);
+          if (pickup == ERR_NOT_IN_RANGE) {
+            var startCpu = Game.cpu.getUsed();
+            creep.moveTo(source, {
+              visualizePathStyle: {stroke: '#ffaa00'}, reusePath: 5
+            });
+
+            console.log( Game.cpu.getUsed() - startCpu ); // 0.2
           }
+        }
+        // if the source is storage/container withdraw from it
+        if ((source.structureType == STRUCTURE_STORAGE ||
+             source.structureType == STRUCTURE_CONTAINER) &&
+             source.store[RESOURCE_ENERGY] > 0)  {
+          let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
+          if (withdraw == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source, {
+              visualizePathStyle: {stroke: '#ffaa00'}, reusePath: 5
+            });
+          }
+        }
+      }
+    } else {
+      creep.memory.source = null;
+      let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: structure => (structure.structureType == STRUCTURE_SPAWN ||
+                              structure.structureType == STRUCTURE_EXTENSION) &&
+                              structure.energy < structure.energyCapacity
       });
       if (target === null) {
         target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-          filter: (structure) => {
-            return structure.structureType == STRUCTURE_STORAGE
-                   _.sum(structure.store) < structure.storeCapacity
-          }
+          filter: structure => structure.structureType == STRUCTURE_TOWER &&
+                               structure.energy < structure.energyCapacity
+        });
+      }
+      if (target === null) {
+        target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          filter: structure => structure.structureType == STRUCTURE_STORAGE &&
+                   	          _.sum(structure.store) < structure.storeCapacity
+
         });
       }
       if (target !== null) {
