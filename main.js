3369d1a2f597944cc367627207b2b06fc6e3fe2a
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-04 00:32:37 +0300

    b

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ let spawnCreeps = [
   {
     role: 'builder',
     priority: 4,
-    goal: 0,
+    goal: 1,
     parameters: { harvesting: false },
     bodyParts: { move: 8, carry: 4, work: 4 }
   },
