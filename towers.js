7b99a93eac483ce961d13e44bfe41d21ff8496ff
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-03 14:51:15 +0300

    new towers code

--- towers.js
+++ towers.js
@@ -1,30 +1,38 @@
 var towers = {
+
   run: function() {
     let towers = Game.spawns.Spawn1.room.find(FIND_MY_STRUCTURES,
         { filter: { structureType: STRUCTURE_TOWER } }
     );
-    let hostiles = Game.spawns.Spawn1.room.find(FIND_HOSTILE_CREEPS);
-    if (hostiles.length) {
-      towers.forEach(tower => tower.attack(hostiles[0]));
-      return;
-    }
-    let hurtCreeps = Game.spawns.Spawn1.room.find(FIND_MY_CREEPS,
-      { filter: creep => creep.hits < creep.hitsMax }
-    );
-    if(hurtCreeps.length) {
-      towers.forEach(tower => tower.heal(hurtCreeps[0]));
-      return;
-    }
-    let damagedStructures = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, {
-      filter: structure => structure.hits < structure.hitsMax &&
-              structure.hits < 50000
-    });
-    if(damagedStructures.length) {
-      towers.forEach(tower => {
-        if (tower.energy > tower.energyCapacity / 2) {
-          tower.repair(damagedStructures[0]);
+
+    let towersLength = towers.length;
+    for (let i = 0; i < towersLength; i++) {
+      let tower = towers[i];
+
+      let hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
+      if (hostiles && hostiles.length) {
+        tower.attack(hostile[0]);
+        return;
+      }
+
+      if (tower.energy > tower.energyCapacity/2) {
+        let hurtCreeps = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+            filter: creep => creep.hits < creep.hitsMax
+        });
+        if(hurtCreeps && hurtCreeps.length) {
+          tower.heal(hurtCreeps[0]);
+          return;
         }
-      });
+        
+        let damagedStructure = tower.room.find(FIND_STRUCTURES, {
+          filter: structure => structure.hits < structure.hitsMax &&
+                               structure.hits < 50001
+        });
+        if(damagedStructure && damagedStructure.length) {
+          tower.repair(damagedStructure[0]);
+          return;
+        }
+      }
     }
   }
 };
