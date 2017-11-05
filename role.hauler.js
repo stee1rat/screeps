6d3ed06f539525510fdc952bb36309cd1e0881d7
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-05 20:58:05 +0300

    optimized code

--- role.hauler.js
+++ role.hauler.js
@@ -6,18 +6,41 @@ let roleHauler = {
     }
     if (creep.carry.energy === 0) {
       if (!creep.memory.source) {
-        let source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-          filter: resource => resource.resourceType == RESOURCE_ENERGY
-        });
-        if (!source) {
-          source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: s => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
-          });
+        let source = null;
+
+        // look for available dropped energy
+        if (Memory.droppedEnergy.length) {
+          let droppedEnergy = _.map(Memory.droppedEnergy, e => Game.getObjectById(e));
+          let availableEnergy = _.filter(droppedEnergy, e =>
+            e.amount - _.sum(_.map(Game.creeps, c =>
+              (c.memory.role == 'hauler' && c.memory.source == e.id &&
+               Game.getObjectById(c.memory.source)) &&
+               c.carryCapacity || 0)) > 0
+          );
+          if (availableEnergy) {
+            source = creep.pos.findClosestByPath(availableEnergy);
+          }
         }
+        // look for avaiable energy in containers
+        // containers with more energy are prefered
+        if (!source && Memory.containers.length) {
+          let containers = _.map(Memory.containers, e => Game.getObjectById(e));
+          let availableContainers = _.filter(containers, x =>
+            x.store[RESOURCE_ENERGY] - _.sum(_.map(Game.creeps, c =>
+              (c.memory.role == 'hauler' && c.memory.source == x.id &&
+               Game.getObjectById(c.memory.source)) &&
+               c.carryCapacity || 0)) > 0
+          );
+          if (availableContainers) {
+            source = availableContainers.sort((a, b) =>
+              b.store[RESOURCE_ENERGY] - a.store[RESOURCE_ENERGY])[0];
+          }
+        }
+        // if there are no other energy sources get it from storage
         if (!source) {
-          source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          source = creep.room.find(FIND_STRUCTURES, {
             filter: s => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 0
-          });
+          })[0];
         }
         if (source) {
           creep.memory.source = source.id;
@@ -44,7 +67,7 @@ let roleHauler = {
             //console.log( Game.cpu.getUsed() - startCpu ); // 0.2
           }
         }
-        // if the source is container withdraw
+        // if the source is a container then withdraw
         if (source.structureType == STRUCTURE_CONTAINER)  {
           if (source.store[RESOURCE_ENERGY] > 0) {
             let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
@@ -56,7 +79,7 @@ let roleHauler = {
           }
         }
         //if the source is storage and there are refills needed
-        console.log(Memory.targetsToRefill.length)
+        //console.log(Memory.targetsToRefill.length)
         if (source.structureType == STRUCTURE_STORAGE)  {
           if (source.store[RESOURCE_ENERGY] > 0 && Memory.targetsToRefill.length > 0) {
             let withdraw = creep.withdraw(source, RESOURCE_ENERGY);
