5fabb044d76914f5cf18e84cb0806546a307a1c4
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-06 04:23:10 +0300

    test

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -13,7 +13,8 @@ let roleRemoteHarvester = {
     }
     console.log(creep.pos.roomName, Game.flags[creep.memory.flagName].pos.roomName)
     if (creep.pos.roomName != Game.flags[creep.memory.flagName].pos.roomName) {
-      creep.moveTo(Game.flags[creep.memory.flagName].pos);
+      creep.move(RIGHT);
+      //creep.moveTo(Game.flags[creep.memory.flagName].pos);
     } else {
       //if (creep.pos != ('29,38')){
         creep.move(RIGHT);
