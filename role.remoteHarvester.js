380afb0c3e95a34433d7805ea65886be81cfe0f8
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-06 04:27:25 +0300

    workaround for the jumps between rooms

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -13,7 +13,12 @@ let roleRemoteHarvester = {
     }
     console.log(creep.pos.roomName, Game.flags[creep.memory.flagName].pos.roomName)
     if (creep.pos.roomName != Game.flags[creep.memory.flagName].pos.roomName) {
-      creep.moveTo(Game.flags[creep.memory.flagName].pos);
+      // workaround for the jumps between rooms
+      if(creep.pos.x*creep.pos.y === 0 || creep.pos.x === 49 || creep.pos.y === 49) {
+        creep.moveTo(new RoomPosition(25,25,creep.memory.workInRoom));
+      } else {
+        creep.moveTo(Game.flags[creep.memory.flagName].pos);
+      }
     } else {
       //if (creep.pos != ('29,38')){
         creep.move(RIGHT);
