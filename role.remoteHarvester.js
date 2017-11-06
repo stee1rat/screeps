d353551d490e1e942ce563475d14c9acdab78739
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-06 19:39:13 +0300

    fix

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -11,11 +11,22 @@ let roleRemoteHarvester = {
       creep.memory.init = true;
       return;
     }
-    // workaround for the jumps between rooms
-    console.log(JSON.stringify(creep));
-    if(creep.pos.x === 0 || creep.pos.y === 0 || creep.pos.x === 49 || creep.pos.y === 49) {
-      creep.moveTo(new RoomPosition(25,25,creep.pos.roomName));
+    // workaround for the jumps between rooms    
+    if (creep.pos.x === 0) {
+      creep.move(RIGHT);
     }
+    if (creep.pos.x === 49) {
+      creep.move(LEFT);
+    }
+    if (creep.pos.y === 0) {
+      creep.move(DOWN)
+    }
+    if (creep.pos.y === 49) {
+      creep.move(UP)
+    }
+    /*if(creep.pos.x === 0 || creep.pos.y === 0 || creep.pos.x === 49 || creep.pos.y === 49) {
+      creep.moveTo(new RoomPosition(25,25,creep.pos.roomName));
+    }*/
     if (creep.pos.roomName != Game.flags[creep.memory.flagName].pos.roomName) {
       creep.moveTo(Game.flags[creep.memory.flagName].pos);
     } else {
