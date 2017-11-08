883c29bae815d5d874c9644af346daed0c57b3f9
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-08 14:59:57 +0300

    added profiler

--- role.claimer.js
+++ role.claimer.js
@@ -1,4 +1,4 @@
-let roleClaimer = {
+module.exports = { // role claimer
   run: function(creep) {
     if (creep.spawning) {
       return;
@@ -7,48 +7,11 @@ let roleClaimer = {
       creep.moveTo(new RoomPosition(25, 25, creep.memory.roomName));
       return;
     }
-    /*if (!creep.memory.controller) {
-      let controller = this.getRoomController(creep);
-      if (controller.length) {
-        creep.memory.controller = controller[0].id;
-      }
-    }*/
-    //if (creep.memory.controller) {
-      let claim = creep.claimController(creep.room.controller);
-      console.log(claim);
-      if (claim == ERR_NOT_IN_RANGE) {
-        let move = creep.moveTo(creep.room.controller);
-        console.log(move);
-      }
-  //  }
+    let claim = creep.claimController(creep.room.controller);
+    console.log(claim);
+    if (claim == ERR_NOT_IN_RANGE) {
+      let move = creep.moveTo(creep.room.controller);
+      console.log(move);
+    }
   }
 };
-/*let roleClaimer = {
-  run: function(creep) {
-    if (creep.spawning) {
-      return;
-    }
-    if (creep.pos.roomName != creep.memory.roomName) {
-      creep.moveTo(new RoomPosition(25, 25, creep.memory.roomName));
-      return;
-    }
-    if (!creep.memory.controller) {
-      let controller = this.getRoomController(creep);
-      if (controller.length) {
-        creep.memory.controller = controller[0].id;
-      }
-    }
-    if (creep.memory.controller) {
-      let controller = Game.getObjectById(creep.memory.controller);
-      if (!creep.claimController(creep.room.controller)) {
-        console.log(creep.moveTo(controller));
-      }
-    }
-  },
-  getRoomController: function(creep) {
-    return creep.room.find(FIND_STRUCTURES, {
-      filter: s => s.structureType == STRUCTURE_CONTROLLER
-    });
-  }
-};*/
-module.exports = roleClaimer;
