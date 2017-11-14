164419e5c91220560cecba7e71678faa8b1871da
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-14 09:22:11 +0300

    update

--- main.js
+++ main.js
@@ -470,6 +470,7 @@ profiler.wrap(function() {
     if(creep.memory.role == 'upgrader') {
       roleUpgrader.run(creep);
     }
+    console.log(creep.name + ' CPU: ' + (Game.cpu.getUsed() - creepCPU));
     upgraderCPU += Game.cpu.getUsed() - creepCPU;
     creepCPU = Game.cpu.getUsed();
     if(creep.memory.role == 'builder') {
