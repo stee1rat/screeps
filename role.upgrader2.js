bf2a32a8e98164d4b03a599a560d72b5cf15d6df
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-14 10:59:45 +0300

    update

---
+++ role.upgrader2.js
@@ -0,0 +1,32 @@
+// Role upgrader
+// https://screepsworld.com/2017/09/screeps-tutorial-handling-creep-roles-with-a-state-machine/
+// Upgrader role implementation using the state machine from the tutorial above
+
+module.exports = {
+
+  const STATE_SPAWNING = 0;
+  const STATE_MOVING = 1;
+  const STATE_HARVESTING = 2;
+  const STATE_DEPOSITING = 3;
+
+  let run = function(creep) {
+    if(!creep.memory.state) {
+      creep.memory.state = STATE_SPAWNING;
+    }
+    switch(creep.memory.state) {
+      case STATE_SPAWNING:
+        //runSpawning(creep);
+        console.log('IT"S WORKING')
+        break;
+      case STATE_MOVING:
+        runMoving(creep);
+        break;
+      case STATE_HARVESTING:
+        runHarvesting(creep);
+        break;
+      case STATE_DEPOSITING:
+        runDepositing(creep);
+        break;
+    }
+  }
+};
