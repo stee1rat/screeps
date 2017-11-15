e31f08c91263a0fe48332a627ab7ae2747fb4cd7
Vasiliy Aboymov <aboymov@gmail.com>
2017-11-15 11:04:28 +0300

    update

--- role.upgrader2.js
+++ role.upgrader2.js
@@ -3,12 +3,11 @@
 // https://screepsworld.com/2017/09/screeps-tutorial-handling-creep-roles-with-a-state-machine/
 
 const STATE_SPAWNING = 0;
-const STATE_MOVING = 1;
 const STATE_HARVESTING = 2;
 const STATE_UPGRADING = 3;
 
 module.exports = {
-  run(creep) {
+  run: function(creep) {
     if(!creep.memory.state) {
       creep.memory.state = STATE_SPAWNING;
     }
@@ -16,44 +15,80 @@ module.exports = {
       case STATE_SPAWNING:
         this.runSpawning(creep);
         break;
-      case STATE_MOVING:
-        this.runMoving(creep);
+      case STATE_UPGRADING:
+        this.runUpgrading(creep);
         break;
       case STATE_HARVESTING:
         this.runHarvesting(creep);
         break;
-      case STATE_FIND_ENERGY_SOURCE:
-        this.findEnergySource(creep);
-        break;
-      /*case STATE_UPGRADING:
-        this.runUpgrading(creep);
-        break;*/
     }
   },
-  runSpawning(creep) {
-    if(!creep.spawning) {
-      creep.memory.state = STATE_HARVESTING;
-      this.run(creep);
-      return;
-    }
+  moveTo: function(creep, target) {
+    creep.moveTo(target, {
+      visualizePathStyle: {stroke: '#ffaa00'}, reusePath: 5
+    });
+  },
+  runSpawning: function(creep) {
     if (!creep.memory.init) {
       creep.memory.controller = creep.room.controller.id;
       creep.memory.init = true;
     }
-  },
-  runHarvesting(creep) {
-    if (!creep.memory.source) {
-      creep.memory.source = this.findEnergySource(creep);
+    if(!creep.spawning) {
+      creep.memory.state = STATE_UPGRADING;
+      this.run(creep);
+      return;
     }
   },
-  findEnergySource(creep) {
+  runUpgrading: function(creep) {
+    if (creep.carry.energy === 0) {
+      creep.memory.state = STATE_HARVESTING;
+      this.run(creep);
+      return;
+    }
+    const controller = Game.getObjectById(creep.memory.controller);
+    if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+      this.moveTo(creep, controller);
+    }
+  },
+  runHarvesting: function(creep) {
+    if (!creep.memory.source) {
+      this.findEnergySource(creep);
+    }
+    const source = Game.getObjectById(creep.memory.source);
+    if (!source) {
+      // if the source no longer exists (has been picked up), find another one
+      creep.memory.source = false;
+      this.run(creep);
+      return;
+    }
+    if (source.resourceType) {
+      let result = creep.pickup(source);
+    }
+    if (source.structureType)  {
+      let result = creep.withdraw(source, RESOURCE_ENERGY);
+    }
+    if (source.ticksToRegeneration) {
+      let result = creep.harvest(source);
+    }
+    if (result == ERR_NOT_ENOUGH_RESOURCES) {
+      creep.memory.source = false;
+      this.run(creep);
+      return;
+    }
+    if (result == ERR_NOT_IN_RANGE) {
+      this.moveTo(creep, source);
+    }
+    if (creep.carry.energy == creep.carryCapacity) {
+      delete creep.memory.source;
+      creep.memory.state = STATE_UPGRADING;
+      this.run(creep);
+      return;
+    }
+  },
+  findEnergySource: function(creep) {
     const roomMemory = Memory.rooms[creep.room.name];
-
-    let source = null;
-
     // look for available dropped energy
     if (roomMemory.droppedEnergy.length) {
-      //const energy = _.map(Memory.droppedEnergy, e => Game.getObjectById(e));
       const energyAvailable = _.filter(roomMemory.droppedEnergy, function(e) {
         // find all creeps that have this energy as a source
         const creeps = _.filter(Game.creeps, c => c.memory.source == e);
@@ -65,42 +100,41 @@ module.exports = {
 
       if (energyAvailable.length) {
         source = creep.pos.findClosestByPath(energyAvailable);
+        creep.memory.source = source.id;
+        return;
       }
     }
     // take energy from container or storage, whichever is closer
-    if (!source) {
-      let containersAndStorage = [];
-      if (roomMemory.containers.length) {
-        let containers = _.map(roomMemory.containers, e => Game.getObjectById(e));
-        let availableContainers = _.filter(containers, x =>
-          x.store[RESOURCE_ENERGY] - _.sum(_.map(Game.creeps, c =>
-            //(c.memory.role == 'hauler' && c.memory.source == x.id &&
-            (c.memory.source == x.id &&
-             Game.getObjectById(c.memory.source)) &&
-             c.carryCapacity || 0)) > 0
-        );
-        if (availableContainers) {
-            containersAndStorage = containersAndStorage.concat(availableContainers);
-        }
-      }
-      if (roomMemory.storageID) {
-        let storage = Game.getObjectById(Memory.storageID);
-        if (storage.store[RESOURCE_ENERGY] > 0) {
-          containersAndStorage = containersAndStorage.concat(storage);
-        }
-      }
-      if (containersAndStorage.length) {
-        source = creep.pos.findClosestByPath(containersAndStorage);
+    let containersAndStorage = [];
+    if (roomMemory.containers.length) {
+      let containers = _.map(roomMemory.containers, e => Game.getObjectById(e));
+      let availableContainers = _.filter(containers, x =>
+        x.store[RESOURCE_ENERGY] - _.sum(_.map(Game.creeps, c =>
+          (c.memory.source == x.id &&
+           Game.getObjectById(c.memory.source)) &&
+           c.carryCapacity || 0)) > 0
+      );
+      if (availableContainers) {
+          containersAndStorage = containersAndStorage.concat(availableContainers);
       }
     }
-    // if everything is empty, lets go mining
-    if (!source) {
-      if (creep.getActiveBodyparts(WORK) > 0) {
-        source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    if (roomMemory.storageID) {
+      let storage = Game.getObjectById(Memory.storageID);
+      if (storage.store[RESOURCE_ENERGY] > 0) {
+        containersAndStorage = containersAndStorage.concat(storage);
       }
     }
-    if (source) {
+    if (containersAndStorage.length) {
+      const source = creep.pos.findClosestByPath(containersAndStorage);
       creep.memory.source = source.id;
+      return;
+    }
+    if (creep.getActiveBodyparts(WORK) > 0) {
+      const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      if (source) {
+        creep.memory.source = source.id;
+        return;
+      }
     }
   }
 };
